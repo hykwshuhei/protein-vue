6b9da4c6ef08a370820fe374a22e4f96e31717f2
早川修平 <hykwshuhei@hayakawashuuheinoMacBook-Air.local>
2023-02-13 14:48:06 +0900

    詳細ページに数量と合計金額追加

--- src/views/ItemDetail.vue
+++ src/views/ItemDetail.vue
@@ -13,7 +13,15 @@ export default {
     return {
       item: [],
       flavor: [],
+      countity: 1,
+      price: "",
     };
+  },
+  watch: {
+    countity() {
+      this.price = this.item[0].price;
+      this.price *= this.countity;
+    },
   },
   created: function () {
     (async () => {
@@ -31,6 +39,8 @@ export default {
       this.flavor = await arrFlavor;
       console.log(this.flavor);
       console.log(this.item);
+
+      this.price = item[0].price;
     })();
   },
 };
@@ -54,6 +64,9 @@ export default {
             <h1 class="text-gray-900 text-3xl title-font font-medium">
               {{ item[0].name }}
             </h1>
+            <h2 class="text-gray-900 text-xl title-font font-medium mt-3">
+              ¥ {{ item[0].price }}円
+            </h2>
             <div class="flex mb-4"></div>
             <p class="leading-relaxed">{{ item[0].description }}</p>
             <div
@@ -85,10 +98,40 @@ export default {
                   </span>
                 </div>
               </div>
+              <div class="flex items-center">
+                <span class="mr-3 ml-3">数量</span>
+                <div class="relative">
+                  <select
+                    v-model="countity"
+                    class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base pl-3 pr-10"
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                  <span
+                    class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center"
+                  >
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-4 h-4"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
+                  </span>
+                </div>
+              </div>
             </div>
             <div class="flex">
               <span class="title-font font-medium text-2xl text-gray-900"
-                >¥ {{ item[0].price }}円</span
+                >合計：¥ {{ price }}円</span
               >
               <button
                 class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded"
