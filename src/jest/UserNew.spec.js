c5bba2ab413bfa9bea7fb52d93ed18e456fba4a5
早川修平 <hykwshuhei@hayakawashuuheinoMacBook-Air.local>
2023-03-08 17:55:54 +0900

    テスト通るように修正 UserNew.spec.js

--- src/jest/UserNew.spec.js
+++ src/jest/UserNew.spec.js
@@ -5,6 +5,6 @@ import UserNew from "../views/UserNew.vue";
 describe("UserNew", () => {
   const wrapper = mount(UserNew);
   it("button要素の存在を確認", () => {
-    expect(wrapper.contains("button")).toBe(true);
+    expect(wrapper.exists("button")).toBe(true);
   });
 });
